d5080746ce0b4e0828af9c1e48e0f6afde601ba6
Blake Redwolf <blakethedead@gmail.com>
2017-09-28 23:38:45 -0700

    Issue pulling products from db and displaying them through hbs, check syntax on index.hbs

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,11 @@ var Product = require('../models/product');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   Product.find(function(err, docs) {
+    var productChunks = [];
+    var chunkSize = 3;
+    for (var i = 0; i < docs.length; i += chunkSize) {
+      productChunks.push(docs.slice(i, i + chunkSize));
+    }
     res.render('shop/index', { title: 'Node & Express Shopping Cart', products: docs });
   });
 });
