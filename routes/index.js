3681c81fa3d67651f51a431979aedcf4f181d560
Blake Redwolf <blakethedead@gmail.com>
2017-11-26 20:38:37 -0800

    Fixed bug preventing signup page from not showing up

--- routes/index.js
+++ routes/index.js
@@ -23,7 +23,7 @@ router.get('/', function(req, res, next) {
 
 router.get('/user/signup', function(req, res, next) {
   var messages= req.flash('error');
-  res.render('user/signup', {csrfToken: req.csrfToken(), messages: massages, hasErrors: messages.length > 0});
+  res.render('user/signup', {csrfToken: req.csrfToken(), messages: messages, hasErrors: messages.length > 0});
 });
 
 router.post('/user/signup', passport.authenticate('local.signup', {
