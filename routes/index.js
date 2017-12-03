566b1f68deae83fa2f4e5a755802d10921bc5369
Blake Redwolf <blakethedead@gmail.com>
2017-12-03 03:28:29 -0800

    Updated checkout.js form and added route)

--- routes/index.js
+++ routes/index.js
@@ -39,6 +39,14 @@ router.get('/shopping-cart', function(req, res, next) {
   res.render('shop/shopping-cart', {products: cart.generateArray(), totalPrice: cart.totalPrice});
 });
 
+router.get('/checkout', function(req, res, next) {
+  if (!req.session.cart) {
+    return res.redirect('/shopping-cart');
+  }
+  var cart = new Cart(req.session.cart);
+  res.render('shop/checkout', {total: cart.totalPrice});
+});
+
 module.exports = router;
 
 // LF@#10 Route Grouping & Protection (Middleware) 0.05
